d2c02c37c05bb58b2227c6a0acad855caf36cc2f
ederp <epmutley@gmail.com>
2024-04-10 11:41:25 -0300

    Criação das classes DTO

---
+++ backend/src/main/java/com/example/planisa/dto/CountryDTO.java
@@ -0,0 +1,19 @@
+package com.example.planisa.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude
+public class CountryDTO {
+
+    private String country;
+    private String region;
+}
---
+++ backend/src/main/java/com/example/planisa/dto/DeathsDTO.java
@@ -0,0 +1,22 @@
+package com.example.planisa.dto;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude
+public class DeathsDTO {
+
+    private LocalDate date;
+    private String totalCases;
+    private String newCases;
+}
