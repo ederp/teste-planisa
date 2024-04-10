63d95c581d7f36dee623b385cddca745caf617e2
ederp <epmutley@gmail.com>
2024-04-10 09:58:42 -0300

    Criação do diretório de backend criado com o Spring Boot

---
+++ backend/src/main/java/com/example/planisa/PlanisaApplication.java
@@ -0,0 +1,13 @@
+package com.example.planisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanisaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/planisa/PlanisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.planisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
