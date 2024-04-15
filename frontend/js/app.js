0d7c7f8587d457da2f1859eb5c4301264776671c
ederp <epmutley@gmail.com>
2024-04-15 10:33:19 -0300

    Ajustes de usabilidade no frontend

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -4,3 +4,14 @@ const selectPaises1 = document.getElementById("country1");
 const selectPaises2 = document.getElementById("country2");
 new DataInput(selectPaises1);
 new DataInput(selectPaises2);
+
+const form = document.getElementById('totalCasesDeathsForm');
+const inputs = form.querySelectorAll('.country-input, #fromDate, #toDate, #dataType');
+const getDataBtn = document.getElementById('getDataBtn');
+
+inputs.forEach(input => {
+    input.addEventListener('change', () => {
+        const allFilled = Array.from(inputs).every(input => input.value.trim() !== '');
+        getDataBtn.disabled = !allFilled;
+    });
+});
